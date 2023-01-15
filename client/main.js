39e7b2b3cb9b779e9442075ba0eac7c7e80d98c3
Matthew Sellers <Matthewsellers20@gmail.com>
2023-01-14 21:13:02 -0700

    second

--- client/main.js
+++ client/main.js
@@ -9,3 +9,16 @@ const getCompliment = () => {
 };
 
 complimentBtn.addEventListener('click', getCompliment)
+
+
+const fortuneBtn = document.getElementById("fortuneButton")
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => { 
+            const data = res.data;
+            alert(data);
+    });
+};
+
+fortuneBtn.addEventListener('click', getFortune)
--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,13 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
-
+    },   
+    getFortune: (req, res) => {
+        const fortune = ["A beautiful, smart, and loving person will be coming into your life.", "A dubious friend may be an enemy in camouflage.", "A faithful friend is a strong defense.", "A fresh start will put you on your way.", "A friend is a present you give yourself."];
+      
+        let randomIndex = Math.floor(Math.random() * fortune.length);
+        let randomFortune = fortune[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }   
 }
